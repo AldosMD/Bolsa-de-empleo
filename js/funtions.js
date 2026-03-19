f150194e828a51a471b91f3e7175990a1bd458b1
AldosMD <Aldo.montes@cua.uam.mx>
2026-03-19 09:58:09 -0600

    info de la vacante

---
+++ js/funtions.js
@@ -0,0 +1,15 @@
+function promotor(){
+    window.location.href="vacantes/promotor.html";
+}
+
+function callcenter(){
+    window.location.href="vacantes/callcenter.html"
+}
+
+function ayudante(){
+    window.location.href="vacantes/ayudante.html"
+}
+
+function seguridad(){
+    window.location.href="vacantes/seguridad.html"
+}
